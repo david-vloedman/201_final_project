bbac7dd8665a09ea099db1a8bbef89efd5379506
mason <mwalker0909>
2019-10-14 15:42:06 -0700

    creating beer object

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+var beers = [];
+
+var Beer = function(brand,name,flav_profile,description) {
+  this.brand = brand;
+  this.name = name;
+  this.flav_profile = flav_profile;
+  this.description = description;
+  beers.push(this);
+}
