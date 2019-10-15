67ef7b6da7e3dde824795604ae00d09f5e135669
David Vloedman <djvloedman@gmail.com>
2019-10-15 10:01:09 -0700

    Add Persistence Manager

--- js/app.js
+++ js/app.js
@@ -8,11 +8,7 @@ function User (name, age){
 
 User.prototype.isLegal = function(){
   return this.age >= this.legalAge;
-}
-
-
-
-
+};
 
 var beers = [];
 
@@ -22,5 +18,51 @@ var Beer = function(brand,name,flav_profile,description) {
   this.flav_profile = flav_profile;
   this.description = description;
   beers.push(this);
+};
+
+
+var persistenceManager = {
+
+  // pass in user object to be saved
+  saveUser: function(user){
+    var toStore = JSON.stringify(user);
+    localStorage.setItem('user', toStore);
+  },
+
+  // returns a stored user
+  getUser: function(){
+    return JSON.parse(localStorage.getItem('user'));
+  },
+
+ 
+  // pass in a history object to be saved
+  storeHistoric: function(historicData){
+    var toStore = JSON.stringify(historicData);
+    localStorage.setItem('history', toStore);
+  },
+  // retrieve history from local storage
+  getHistoric: function(){
+    return JSON.parse(localStorage.getItem('history'));
+  }
+};
+
+
+function SuggestionHistory(previous){
+  this.historyData = previous;
+  if(previous === null) this.historyData(this.fabricateHistory());  
 }
+
+SuggestionHistory.prototype.addBeer(beer){
+  this.historyData.push(beer);
+}
+
+SuggestionHistory.prototype.fabricateHistory = function(){
+
+};
+
+
+
+var alltime = new SuggestionHistory();
+
+
 
