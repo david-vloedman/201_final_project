6d1263735d71957e80b9a8566b2d543669b20e93
David Vloedman <djvloedman@gmail.com>
2019-10-15 13:04:58 -0700

    Eliminate all run-time errors

--- js/app.js
+++ js/app.js
@@ -24,10 +24,10 @@ User.prototype.isLegal = function(){
 function RenderResults(beer){
   this.list = this.getList();
   this.img = this.getImg();
-};
+}
 
 RenderResults.prototype.getList = function(){
-  this.list = getElementbyID('beer');
+  this.list = Document.getElementbyID('beer');
 };
 
 RenderResults.prototype.getImg = function(){
@@ -61,32 +61,32 @@ var Beer = function(brand,name, type, flav_profile, description) {
   this.description = description;
   beers.push(this);
 
-}
+};
 
 
-var BeerFactory = function(beerType, flavorAle, flavorLager, flavorDark) {
-  var beerType = [ale, lager, dark];
+var BeerFactory = function() {
+  var beerType = ['ale', 'lager', 'dark'];
   var flavorAle = ['Warm and Malty', 'Crisp and Light', 'Hoppy', 'Sour'];
   var flavorLager = ['Light and Refreshing', 'Deep and Malty', 'Light and Hoppy'];
   var flavorDark = ['Coffee', 'Chocolate-y'];
   var beerBrands = ['Boneyard', 'Goodlife', 'Deschutes Brewery', 'Sunriver Brewing', '10Barrel', 'Bend Brewing Company', 'Crux'];
   var finalFlavors = ['Amber Ale', 'Pale Ale', 'Indian Pale Ale', 'Sour', 'Pale Lager', 'Dark Lager', 'Pilsner', 'Stout', 'Porter'];
-  
 
-  BeerFactory.prototype.createBeerList(); {
-   beer.push(new Beer(beerBrands[0], 'Rojo Diablo Amber Ale', beerType[0],flavorAle[0], diabloRojo));
-   beer.push(new Beer(beerBrands[1], 'Sweet As Pacific Pale', beerType[0],flavorAle[1], sweetAs));
-   beer.push(new Beer(beerBrands[3], 'Vicious Mosquito', beerType[0], flavorAle[2], viciousMosq));
-   beer.push(new Beer(beerBrands[5], 'ChingChing Sour', beerType[0], flavorAle[3], chingChingSour));
-   beer.push(new Beer(beerBrands[2], 'Pacific Wonderland', beerType[1], flavorLager[0], pacificWond));
-   beer.push(new Beer(beerBrands[5], 'Bend Black Diamond Lager', beerType[1], flavorLager[1], bendBlackDiamond));
-   beer.push(new Beer(beerBrands[6], 'Pilsner', beerType[1], flavorLager[2], cruxPils));
-   beer.push(new Beer(beerBrands[4], 'Dutch Delight', beerType[2], flavorDark[0], dutchDelight));
-   beer.push(new Beer(beerBrands[2], 'Black Butte Porter', beerType[2], flavorDark[1], blackButte));
+
+  BeerFactory.prototype.createBeerList = function(){
+    new Beer(beerBrands[0], 'Rojo Diablo Amber Ale', beerType[0],flavorAle[0], diabloRojo);
+    new Beer(beerBrands[1], 'Sweet As Pacific Pale', beerType[0],flavorAle[1], sweetAs);
+    new Beer(beerBrands[3], 'Vicious Mosquito', beerType[0], flavorAle[2], viciousMosq);
+    new Beer(beerBrands[5], 'ChingChing Sour', beerType[0], flavorAle[3], chingChingSour);
+    new Beer(beerBrands[2], 'Pacific Wonderland', beerType[1], flavorLager[0], pacificWond);
+    new Beer(beerBrands[5], 'Bend Black Diamond Lager', beerType[1], flavorLager[1], bendBlackDiamond);
+    new Beer(beerBrands[6], 'Pilsner', beerType[1], flavorLager[2], cruxPils);
+    new Beer(beerBrands[4], 'Dutch Delight', beerType[2], flavorDark[0], dutchDelight);
+    new Beer(beerBrands[2], 'Black Butte Porter', beerType[2], flavorDark[1], blackButte);
   }
-}
+};
 
-//Array of questions. 
+//Array of questions.
 var questionArr = ['What\'s your type?','What\'s your flavor?'];
 
 // ************************************************************************
@@ -108,7 +108,7 @@ var persistenceManager = {
     return JSON.parse(localStorage.getItem('user'));
   },
 
- 
+
   // pass in a history object to be saved
   storeHistoric: function(historicData){
     var toStore = JSON.stringify(historicData);
@@ -128,10 +128,10 @@ var persistenceManager = {
 
 function ResultsHistory(){
   this.historyData = persistenceManager.getHistoric();
-  
+
   if(this.historyData === null)
   {
-    
+
     this.fabricateHistory();
   }
 }
@@ -155,7 +155,7 @@ ResultsHistory.prototype.fabricateHistory = function(){
 
 ResultsHistory.prototype.packageForChart = function(){
   var names = [];
-  var package = [];
+  var packaged = [];
   this.historyData.forEach(beer => {
     names.push(beer.name);
   });
@@ -166,7 +166,7 @@ ResultsHistory.prototype.packageForChart = function(){
 
 
 !function(){
-
+  var factory = new BeerFactory();
 }();
 
 
