4551d7983de60f5bf606b5ae3634c74359fb29ea
nataliealway <nlmaxwell@gmail.com>
2019-10-15 09:34:16 -0700

    created object to hold beer types and flavors

--- js/app.js
+++ js/app.js
@@ -8,3 +8,19 @@ var Beer = function(brand,name,flav_profile,description) {
   this.description = description;
   beers.push(this);
 }
+
+//beerFactory, -->2 arrays : all types beers (3) array2 flavors (for all options)
+
+var beerFactory = function(beerType, flavorAle, flavorLager, flavorDark) {
+  var beerType = [ale, lager, dark];
+  var flavorAle = ['Warm and Malty', 'Crisp and Light', 'Hoppy', 'Sour'];
+  var flavorLager = ['Crisp and Light', 'Deep and Malty', 'Light and Hoppy'];
+  var flavorDark = ['Coffee', 'Chocolate-y'];
+
+  beerFactory.prototype.createBeerList(); {
+    this.beerType = beertype; 
+    this.flavorAle = flavorAle;
+    this.flavorLager = flavorLager;
+    this.flavorDark = flavorDark;
+  }
+}
